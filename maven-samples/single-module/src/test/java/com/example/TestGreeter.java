a2e89ad35ae8c5f759d5b59d726a6db164bd7e34
sasikumar <sasi.it23@gmail.com>
2024-03-24 00:03:40 +0530

    Update TestGreeter.java for trigeer build by pool scm

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,7 +27,6 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
   
-
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
